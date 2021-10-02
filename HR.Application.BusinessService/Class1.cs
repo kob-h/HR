eff57aa8b14838563ae79c4a13e0cf2493044c2e
kob-h <kobi.hagmi@gmail.com>
2021-10-02 18:35:35 +0300

    project restructuring

---
+++ HR.Application.BusinessService/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BenchmarkDotNet.Attributes;
+
+namespace HR.Application.BusinessService
+{
+    [MemoryDiagnoser]
+    public class Class1
+    {
+        [Benchmark]
+        public void Method()
+        {
+
+        }
+    }
+}
--- HR.Application.BusinessService/Employees/GetAllEmployees.cs
+++ HR.Application.BusinessService/Employees/GetAllEmployees.cs
@@ -4,15 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using AutoMapper;
+using BenchmarkDotNet.Attributes;
 using HR.Application.BusinessService.Interfaces;
 using HR.Application.Dtos;
 using HR.Persistence.Database;
-using HR.Persistence.Entities;
 using HR.Persistence.Repositories.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 namespace HR.Application.BusinessService.Employees
 {
+    [MemoryDiagnoser]
     public class GetAllEmployees : IGetAllEmployees
     {
         private readonly IMapper _mapper;
@@ -25,6 +26,7 @@ namespace HR.Application.BusinessService.Employees
             _mapper = mapper;
             _employeeRepository = employeeRepository;
         }
+        [Benchmark]
         public async Task<List<EmployeeDto>> Execute()
         {
             var employeesEntity = await _employeeRepository
